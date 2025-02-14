6d1fc7ffb9dd7fb97a5e6d521f72191e6a611d0b
ClyZeR1 <sergeistarodyb@gmail.com>
2025-02-14 12:47:33 +0200

    Add files via upload

---
+++ ConsoleApp23/Calculate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Calculate
+    {
+        private double field1;
+        private double field2;
+
+        public Calculate()
+        {
+            field1 = 0;
+            field2 = 0;
+        }
+
+        public Calculate(double value)
+        {
+            field1 = value;
+            field2 = value;
+        }
+
+        public Calculate(double value1, double value2)
+        {
+            field1 = value1;
+            field2 = value2;
+        }
+
+        public string GetInfo()
+        {
+            return $"Field1: {field1}, Field2: {field2}";
+        }
+
+        public double CalculateSquareRootOfProduct()
+        {
+            return Math.Sqrt(field1 * field2);
+        }
+
+        public void SetValues(double value1, double value2)
+        {
+            field1 = value1;
+            field2 = value2;
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calculate obj1 = new Calculate();
+            obj1.SetValues(4.0, 9.0);
+            Console.WriteLine(obj1.GetInfo());
+            Console.WriteLine("Корень " + obj1.CalculateSquareRootOfProduct());
+
+            Calculate obj2 = new Calculate();
+            obj2.SetValues(16.0, 25.0);
+            Console.WriteLine(obj2.GetInfo());
+            Console.WriteLine("Корень " + obj2.CalculateSquareRootOfProduct());
+
+            Console.WriteLine("Введите два числа: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Calculate obj3 = new Calculate(input1, input2);
+            Console.WriteLine(obj3.GetInfo());
+            Console.WriteLine("Корень: " + obj3.CalculateSquareRootOfProduct());
+            Console.ReadKey();
+        }
+    }
+}
